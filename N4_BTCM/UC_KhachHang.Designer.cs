dedeedc9b6bd13c1cf3a6b981788844db06dfba7
kalinn27 <khanhlinh02072003@gmail.com>
2025-05-14 10:42:43 +0700

    gd khach hang

--- N4_BTCM/UC_KhachHang.Designer.cs
+++ N4_BTCM/UC_KhachHang.Designer.cs
@@ -187,6 +187,7 @@
             this.label2.Size = new System.Drawing.Size(74, 23);
             this.label2.TabIndex = 2;
             this.label2.Text = "89,000 đ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // pictureBox2
             // 
@@ -217,16 +218,17 @@
             this.label3.Size = new System.Drawing.Size(74, 23);
             this.label3.TabIndex = 2;
             this.label3.Text = "89,000 đ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(41, 278);
+            this.label4.Location = new System.Drawing.Point(36, 278);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(224, 28);
+            this.label4.Size = new System.Drawing.Size(232, 28);
             this.label4.TabIndex = 1;
-            this.label4.Text = "Thuốc trừ sâu Ruồi Vàng";
+            this.label4.Text = "Thuốc trừ bệnh Vil Ấn Độ";
             // 
             // pictureBox4
             // 
@@ -267,17 +269,18 @@
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(74, 23);
             this.label5.TabIndex = 2;
-            this.label5.Text = "89,000 đ";
+            this.label5.Text = "79,000 đ";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(41, 278);
+            this.label6.Location = new System.Drawing.Point(40, 278);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(224, 28);
+            this.label6.Size = new System.Drawing.Size(229, 28);
             this.label6.TabIndex = 1;
-            this.label6.Text = "Thuốc trừ sâu Ruồi Vàng";
+            this.label6.Text = "Chế phẩm diệt côn trùng";
             // 
             // pictureBox6
             // 
@@ -318,17 +321,18 @@
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(74, 23);
             this.label7.TabIndex = 2;
-            this.label7.Text = "89,000 đ";
+            this.label7.Text = "79,000 đ";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(41, 278);
+            this.label8.Location = new System.Drawing.Point(30, 278);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(224, 28);
+            this.label8.Size = new System.Drawing.Size(245, 28);
             this.label8.TabIndex = 1;
-            this.label8.Text = "Thuốc trừ sâu Ruồi Vàng";
+            this.label8.Text = "Thuốc trừ bệnh Nice Super";
             // 
             // pictureBox8
             // 
@@ -388,19 +392,20 @@
             this.label9.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.Location = new System.Drawing.Point(115, 311);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(74, 23);
+            this.label9.Size = new System.Drawing.Size(83, 23);
             this.label9.TabIndex = 2;
-            this.label9.Text = "89,000 đ";
+            this.label9.Text = "119,000 đ";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(41, 278);
+            this.label10.Location = new System.Drawing.Point(27, 278);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(224, 28);
+            this.label10.Size = new System.Drawing.Size(246, 28);
             this.label10.TabIndex = 1;
-            this.label10.Text = "Thuốc trừ sâu Ruồi Vàng";
+            this.label10.Text = "Thuốc trừ sâu Comda Gold";
             // 
             // pictureBox10
             // 
@@ -441,17 +446,18 @@
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(74, 23);
             this.label11.TabIndex = 2;
-            this.label11.Text = "89,000 đ";
+            this.label11.Text = "99,000 đ";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label12
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(41, 278);
+            this.label12.Location = new System.Drawing.Point(57, 278);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(224, 28);
+            this.label12.Size = new System.Drawing.Size(186, 28);
             this.label12.TabIndex = 1;
-            this.label12.Text = "Thuốc trừ sâu Ruồi Vàng";
+            this.label12.Text = "Đặc trị Ốc bưu vàng";
             // 
             // pictureBox12
             // 
@@ -490,19 +496,20 @@
             this.label13.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label13.Location = new System.Drawing.Point(115, 311);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(74, 23);
+            this.label13.Size = new System.Drawing.Size(83, 23);
             this.label13.TabIndex = 2;
-            this.label13.Text = "89,000 đ";
+            this.label13.Text = "129,000 đ";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label14
             // 
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label14.Location = new System.Drawing.Point(41, 278);
+            this.label14.Location = new System.Drawing.Point(49, 278);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(224, 28);
+            this.label14.Size = new System.Drawing.Size(195, 28);
             this.label14.TabIndex = 1;
-            this.label14.Text = "Thuốc trừ sâu Ruồi Vàng";
+            this.label14.Text = "Thuốc kích rễ HUMIC";
             // 
             // pictureBox14
             // 
@@ -541,19 +548,20 @@
             this.label15.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label15.Location = new System.Drawing.Point(115, 311);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(74, 23);
+            this.label15.Size = new System.Drawing.Size(83, 23);
             this.label15.TabIndex = 2;
-            this.label15.Text = "89,000 đ";
+            this.label15.Text = "159,000 đ";
+            this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label16
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label16.Location = new System.Drawing.Point(41, 278);
+            this.label16.Location = new System.Drawing.Point(28, 278);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(224, 28);
+            this.label16.Size = new System.Drawing.Size(250, 28);
             this.label16.TabIndex = 1;
-            this.label16.Text = "Thuốc trừ sâu Ruồi Vàng";
+            this.label16.Text = "Thuốc trừ bệnh Tokyo Nhật";
             // 
             // pictureBox16
             // 
